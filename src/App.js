2a26a6e224a1e5115a28c3ed17f038f38fb45dae
Michael <maicol_v16@hotmail.com>
2023-05-23 21:49:49 -0600

    Adding url

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './App.css';
-import Houses from './components/Houses/Houses';
-import NavBar from './components/NavBar/NavBar';
+import "./App.css";
+import Houses from "./components/Houses/Houses";
+import NavBar from "./components/NavBar/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-  function App() {
-   return (
-   <div className="App">
-    <NavBar/>
-   <Houses />
-   </div>
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Houses />
+    </div>
   );
 }
 
